1fd614e54fc80f26ee8509e0d3fcda26b9cff6ef
Honey-bee-code <hanifinida@gmail.com>
2021-03-19 10:47:08 +0800

    wpu hut 2 complete

--- wpu_hut2/js/script.js
+++ wpu_hut2/js/script.js
@@ -1,22 +1,63 @@
-$.getJSON("data/pizza.json", function (hasil) {
-  let menu = hasil.menu;
-  $.each(menu, function (index, data) {
-    // append untuk menambahkan diakhir
-    $("#daftar-menu").append(
-      '<div class="col-md-3" style="margin-bottom: 25px;"><div class="card"><img src="img/menu/' +
-        data.gambar +
-        '" ="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' +
-        data.nama +
-        '</h5><h6 class="card-title">Kategori : ' +
-        data.kategori +
-        '</h6><p class="card-text">' +
-        data.deskripsi +
-        '</p><h5 id="rupiah"> Rp. ' +
-        data.harga +
-        '</h5><a href="#" class="btn btn-primary">Pesan Sekarang!</a></div></div></div>'
-    );
+function allMenu() {
+  $.getJSON("data/pizza.json", function (hasil) {
+    let menu = hasil.menu;
+    $.each(menu, function (index, data) {
+      // append untuk menambahkan diakhir
+      $("#daftar-menu").append(
+        '<div class="col-md-3" style="margin-bottom: 25px;"><div class="card"><img src="img/menu/' +
+          data.gambar +
+          '" ="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' +
+          data.nama +
+          '</h5><h6 class="card-title">Kategori : ' +
+          data.kategori +
+          '</h6><p class="card-text">' +
+          data.deskripsi +
+          '</p><h5 id="rupiah"> Rp. ' +
+          data.harga +
+          '</h5><a href="#" class="btn btn-primary">Pesan Sekarang!</a></div></div></div>'
+      );
+    });
+  });
+}
+
+allMenu();
+
+$(".nav-link").on("click", function () {
+  $(".nav-link").removeClass("active");
+  $(this).addClass("active");
+
+  let kategori = $(this).html();
+  //   console.log(kategori);
+  $("h1").html(kategori);
+//   if (kategori == "All Menu") {
+    
+//     allMenu();
+//     return;
+//   }
+
+  $.getJSON("data/pizza.json", function (hasil) {
+    let menu = hasil.menu;
+    let content = "";
+    $.each(menu, function (index, data) {
+      if (data.kategori == kategori.toLowerCase()) {
+        content +=
+          '<div class="col-md-3" style="margin-bottom: 25px;"><div class="card"><img src="img/menu/' +
+          data.gambar +
+          '" ="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' +
+          data.nama +
+          '</h5><h6 class="card-title">Kategori : ' +
+          data.kategori +
+          '</h6><p class="card-text">' +
+          data.deskripsi +
+          '</p><h5 id="rupiah"> Rp. ' +
+          data.harga +
+          '</h5><a href="#" class="btn btn-primary">Pesan Sekarang!</a></div></div></div>';
+      }
+    });
+    $("#daftar-menu").html(content);
   });
 });
+
 // function rupiah(bilangan) {
 //   var numberString = bilangan.toString(),
 //     sisa = numberString.length % 3,
@@ -29,8 +70,3 @@ $.getJSON("data/pizza.json", function (hasil) {
 //   }
 //   document.write("Rp " + rupiah);
 // }
-
-$(".nav-link").on("click", function () {
-  $(".nav-link").removeClass("active");
-  $(this).addClass("active");
-});
