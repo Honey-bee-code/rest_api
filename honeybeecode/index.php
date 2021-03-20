ccc6656962d9388c7242e42214d7495a58772c9c
Honey-bee-code <hanifinida@gmail.com>
2021-03-20 19:02:30 +0800

    honeybeecode front end

--- honeybeecode/index.php
+++ honeybeecode/index.php
@@ -1,3 +1,8 @@
+<?php
+$curl = curl_init();
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -14,14 +19,14 @@
     <link rel="icon" href="img/bee.png">
 
     <!-- My CSS -->
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style2.css" />
     <title>HoneyBeeCode</title>
   </head>
-  <body id="home" style="background-color: #b5cef5">
+  <body id="home" style="background-image: url(img/begrond2.png)">
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="https://honey-bee-code.github.io/">HoneyBeeCode</a>
+        <a class="navbar-brand" href="#home">HoneyBeeCode</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -32,6 +37,9 @@
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#social">Social Media</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#gallery">Gallery</a>
@@ -59,13 +67,7 @@
           </div>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#b5cef5"
-          fill-opacity="1"
-          d="M0,128L26.7,144C53.3,160,107,192,160,202.7C213.3,213,267,203,320,176C373.3,149,427,107,480,101.3C533.3,96,587,128,640,165.3C693.3,203,747,245,800,218.7C853.3,192,907,96,960,53.3C1013.3,11,1067,21,1120,48C1173.3,75,1227,117,1280,138.7C1333.3,160,1387,160,1413,160L1440,160L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"
-        ></path>
-      </svg>
+      
     </section>
     <!-- Akhir Jumbotron -->
 
@@ -77,6 +79,7 @@
             <h2>About Me</h2>
           </div>
         </div>
+        <hr>
         <div class="row justify-content-center">
           <div class="col-lg-4 text-end">
             <p>
@@ -89,15 +92,66 @@
           </div>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#63a0da"
-          fill-opacity="1"
-          d="M0,128L26.7,149.3C53.3,171,107,213,160,224C213.3,235,267,213,320,218.7C373.3,224,427,256,480,272C533.3,288,587,288,640,256C693.3,224,747,160,800,117.3C853.3,75,907,53,960,80C1013.3,107,1067,181,1120,208C1173.3,235,1227,213,1280,181.3C1333.3,149,1387,107,1413,85.3L1440,64L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"
-        ></path>
-      </svg>
+      
     </section>
     <!-- Akhir About -->
+
+    <!-- Youtube & Instagram -->
+    <section class="social" id="social">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <h2>Social Media</h2>
+          </div>
+        </div>
+        <hr>
+        <div class="row justify-content-center">
+          <div class="col-md-5">
+            <div class="row align-items-center">
+              <div class="col-md-4">
+                <img src="img/me2.jpg" style="height: 150px; width: 150px; object-fit: cover; " class="rounded-circle" alt="">
+              </div>
+              <div class="col-md-8">
+                <h5>Kaligrafi Lombok</h5>
+                <p>1000 Subscribers</p>
+              </div>
+            </div>
+            <div class="row mt-3 pb-3">
+              <div class="col">
+                <div class="ratio ratio-16x9">
+                  <iframe src="https://www.youtube.com/embed/Wk3mx3oM3UE?rel=0" title="YouTube video" allowfullscreen></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="row align-items-center">
+              <div class="col-md-4">
+                <img src="img/me2.jpg" style="height: 150px; width: 150px; object-fit: cover; " class="rounded-circle" alt="">
+              </div>
+              <div class="col-md-8">
+                <h5>@kaligrafi_lombok</h5>
+                <p>1000 Followers</p>
+              </div>
+            </div>
+            <div class="row mt-3 pb-3">
+              <div class="col">
+                <div class="ig-thumbnail">
+                  <img src="img/gallery/1.jpg" alt="">
+                </div>
+                <div class="ig-thumbnail">
+                  <img src="img/gallery/2.jpg" alt="">
+                </div>
+                <div class="ig-thumbnail">
+                  <img src="img/gallery/3.jpg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Akhir Youtube & Instagram -->
 
     <!-- gallery -->
     <section id="gallery">
@@ -105,6 +159,7 @@
         <div class="row text-center mb-3">
           <h2>My Gallery</h2>
         </div>
+        <hr>
         <div class="row justify-content-center">
           <div class="col-md-4 mb-3">
             <div class="card">
@@ -153,13 +208,7 @@
           </div>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#b5cef5"
-          fill-opacity="1"
-          d="M0,224L26.7,186.7C53.3,149,107,75,160,48C213.3,21,267,43,320,90.7C373.3,139,427,213,480,245.3C533.3,277,587,267,640,218.7C693.3,171,747,85,800,69.3C853.3,53,907,107,960,144C1013.3,181,1067,203,1120,181.3C1173.3,160,1227,96,1280,80C1333.3,64,1387,96,1413,112L1440,128L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"
-        ></path>
-      </svg>
+      
     </section>
     <!-- Akhir gallery -->
 
@@ -169,6 +218,7 @@
         <div class="row text-center mb-3">
           <h2>My Project</h2>
         </div>
+        <hr>
         <div class="row justify-content-center">
           <div class="col-md-4 mb-3">
             <div class="card">
@@ -214,13 +264,7 @@
           </div>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#63a0da"
-          fill-opacity="1"
-          d="M0,224L26.7,186.7C53.3,149,107,75,160,48C213.3,21,267,43,320,90.7C373.3,139,427,213,480,245.3C533.3,277,587,267,640,218.7C693.3,171,747,85,800,69.3C853.3,53,907,107,960,144C1013.3,181,1067,203,1120,181.3C1173.3,160,1227,96,1280,80C1333.3,64,1387,96,1413,112L1440,128L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"
-        ></path>
-      </svg>
+      
     </section>
     <!-- Akhir Project -->
 
@@ -232,6 +276,7 @@
             <h2>Contact Me</h2>
           </div>
         </div>
+        <hr>
         <div class="row justify-content-center">
           <div class="col-md-6">
             <div class="alert alert-success alert-dismissible fade show d-none contact-alert" role="alert">
